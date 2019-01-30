fb1d5afadfec1e68971a35409d5c65e73a220ab4
Interative Supply Co <support@interactivesupply.com>
2019-01-30 15:04:07 -0600

    fix open code

--- tail_website_error_log.bat
+++ tail_website_error_log.bat
@@ -1,2 +1,2 @@
 start "" "%ProgramFiles%\Git\git-bash.exe" -c "tail -f /c/src/tmp/logs/website_error.log"
-Start "" "C:\Program Files\Microsoft VS Code\code.exe" "C:\src\tmp\logs\website_error.log"\.
+code "C:\src\tmp\logs\website_error.log"

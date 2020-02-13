f5e3e3b25cfff76bb5eb30e8b0cccdc136af3fde
Brad Linger <blinger@interactivesupply.com>
2020-02-13 11:45:36 -0600

    reverting back database script change

--- deployment/reset_db.sh
+++ deployment/reset_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DB_NAME="$1"
 DB_HOST="$2"
-SSH_HOST="$3"  
+SSH_HOST="$3"   
 REMOTE_DB_FILE_URI="$4"
 
 scp -r -o StrictHostKeyChecking=no $SSH_HOST:$REMOTE_DB_FILE_URI ~/src/tmp/$DB_NAME.sql

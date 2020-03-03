190056716730b2ededce79f55d69f0bbf8383926
Brad Linger <blinger@interactivesupply.com>
2020-03-03 15:12:27 -0600

    refining dns script

--- deployment/dns.sh
+++ deployment/dns.sh
@@ -16,6 +16,7 @@ do
 	fi
 done
 
+#save IP to log for find and replace purposes elsewhere
 
 
 IFS='
@@ -23,17 +24,33 @@ IFS='
 for x in `networksetup -listallnetworkservices`; 
 do 
 echo $x
-	if [ $x == "Wi-Fi" ] ||  [ $x == "Broadcom NetXtreme Gigabit Ethernet Controller" ] ||  [ $x == "Ethernet" ]
+	if [ $x == "Wi-Fi" ] ||  [ $x == "Broadcom NetXtreme Gigabit Ethernet Controller" ] ||  [ $x == "Ethernet"  ] ||  [ $x == "ISC VPN"  ]
 	then
+	  	#nic_ip=$(networksetup -getinfo "$x" | awk 'NR ==2' | awk '{print $3'})
+    	#echo "nic_ip : $nic_ip"
+
 		if [ $STATE == "remote" ]
 		then
-			networksetup -setdnsservers $x 1.1.1.1 192.168.0.1
+			networksetup -setdnsservers $x empty #1.1.1.1 192.168.0.1
 		else
 			networksetup -setdnsservers $x $local_ip
 
 		fi
 	fi
 done
+
+last_local_ip_log=~/src/tmp/last_local_ip.txt
+last_local_ip=$(<$last_local_ip_log)
+sed -i'' -e "s/$last_local_ip/$local_ip/g" /usr/local/etc/dnsmasq.d/development.conf
+
+#log the new IP to this file
+echo $local_ip > $last_local_ip_log
+
+if [ "$local_ip" != "$last_local_ip" ]
+then
+	echo "DNS restart required"
+	sudo brew services restart dnsmasq
+fi
 
 echo "DNS has been changed"
 # sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder

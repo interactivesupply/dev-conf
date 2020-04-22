9f3caac9e197b7c357684bfc6429eaa99a22e985
Brad Linger <blinger@interactivesupply.com>
2020-04-22 17:22:30 -0500

    fixing deployment scripts

--- deployment/reset_db.sh
+++ deployment/reset_db.sh
@@ -3,6 +3,12 @@ DB_NAME="$1"
 DB_HOST="$2"
 SSH_HOST="$3"   
 REMOTE_DB_FILE_URI="$4"
+PORT="$5"
 
-scp -r -o StrictHostKeyChecking=no $SSH_HOST:$REMOTE_DB_FILE_URI ~/src/tmp/$DB_NAME.sql
+if [ -z "$PORT" ]
+then
+	 PORT="22"
+fi
+
+scp -P $PORT -r -o StrictHostKeyChecking=no $SSH_HOST:$REMOTE_DB_FILE_URI ~/src/tmp/$DB_NAME.sql
 mysql -h $DB_HOST -u root -pc0Smo85Log1ca! $DB_NAME < ~/src/tmp/$DB_NAME.sql
